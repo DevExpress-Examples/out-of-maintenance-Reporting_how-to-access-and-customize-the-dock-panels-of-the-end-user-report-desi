ba10aaece6fc72d2a2d66ffd5205d8e7c2a8360a
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/AccessDockPanels/Form1.cs
@@ -0,0 +1,58 @@
+﻿#region #Usings
+using System;
+using System.Windows.Forms;
+using DevExpress.XtraBars.Docking;
+using DevExpress.XtraReports.UserDesigner;
+// ...
+#endregion #Usings
+
+namespace AccessDockPanels {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+#region #Code
+private void button1_Click(object sender, EventArgs e) {
+    // Create an End-User Designer form.
+    XRDesignForm designForm = new XRDesignForm();
+
+    // Open a new blank report in it.
+    designForm.OpenReport(new XtraReport1());
+
+    // Access the Group and Sort panel.
+    GroupAndSortDockPanel groupSort =
+(GroupAndSortDockPanel)designForm.DesignDockManager[DesignDockPanelType.GroupAndSort];
+    groupSort.Visibility = DockVisibility.AutoHide;
+
+    // Access the Script Errors panel.
+    ErrorListDockPanel errorList =
+(ErrorListDockPanel)designForm.DesignDockManager[DesignDockPanelType.ErrorList];
+    errorList.Visibility = DockVisibility.AutoHide;
+
+    // Access the Field List.
+    FieldListDockPanel fieldList =
+(FieldListDockPanel)designForm.DesignDockManager[DesignDockPanelType.FieldList];
+    fieldList.ShowNodeToolTips = false;
+    fieldList.ShowParametersNode = false;
+
+    // Access the Report Explorer.
+    ReportExplorerDockPanel reportExplorer =
+(ReportExplorerDockPanel)designForm.DesignDockManager[DesignDockPanelType.ReportExplorer];
+    reportExplorer.CollapseAll();
+
+    // Access the Property Grid.
+    PropertyGridDockPanel propertyGrid =
+(PropertyGridDockPanel)designForm.DesignDockManager[DesignDockPanelType.PropertyGrid];
+    propertyGrid.ShowCategories = false;
+    propertyGrid.ShowDescription = false;
+
+    // Show the End-User Designer form.
+    designForm.ShowDialog();
+}
+#endregion #Code
+
+    }
+}
+
+
---
+++ Readme.md
@@ -0,0 +1,9 @@
+# How to access and customize the dock panels of the End-User Report Designer
+
+
+<p>This example demonstrates how to access and customize the following dock panels of the <a href="http://documentation.devexpress.com/#XtraReports/CustomDocument10836"><u>End-User Report Designer</u></a>.</p><p>- <a href="http://documentation.devexpress.com/#InterfaceElementsWin/CustomDocument4987"><u>Field List</u></a>;</p><p>- <a href="http://documentation.devexpress.com/#InterfaceElementsWin/CustomDocument4996"><u>Report Explorer</u></a>;</p><p>- <a href="http://documentation.devexpress.com/#InterfaceElementsWin/CustomDocument7402"><u>Group and Sort Panel</u></a>;</p><p>- <a href="http://documentation.devexpress.com/#InterfaceElementsWin/CustomDocument4995"><u>Property Grid</u></a>;</p><p>- <a href="http://documentation.devexpress.com/#InterfaceElementsWin/CustomDocument7491"><u>Scripts Errors Panel</u></a>.</p><br />
+
+
+<br/>
+
+
